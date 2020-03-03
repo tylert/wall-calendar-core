42e509734da0656a51d52a9a409ac35a20115169
Tyler Tidman <tyler.tidman@draak.ca>
2020-03-02 19:38:27 -0500

    Fix up the start date for the count up

--- count.py
+++ count.py
@@ -13,14 +13,14 @@ else:
     b = date(today_date.year, 6, 1) - today_date  # count down to this June
     print('Bv {} days, {:.2f} weeks, {:.2f} months, {:.2f} years'.format(b.days, b.days / 7, b.days / 30, b.days / 365))
 
-# ---==[c]==---
-c = today_date - date(2018, 11, 5)  # count up from start date
-print('C^ {} days, {:.2f} weeks, {:.2f} months, {:.2f} years'.format(c.days, c.days / 7, c.days / 30, c.days / 365))
+# ---==[s]==---
+s = today_date - date(2020, 3, 9)  # count up from start date
+print('S^ {} days, {:.2f} weeks, {:.2f} months, {:.2f} years'.format(s.days, s.days / 7, s.days / 30, s.days / 365))
 
 # ---==[e]==---
 e = today_date - date(2017, 10, 10)  # count up from start date
 print('E^ {} days, {:.2f} weeks, {:.2f} months, {:.2f} years'.format(e.days, e.days / 7, e.days / 30, e.days / 365))
 
 # ---==[d]==---
-# d = date(2017, 10, 10) + timedelta(days=365 * 2) - today_date  # count down to date
+# d = date(2017, 10, 10) + timedelta(days=365 * 2) - today_date  # count down to date two years later
 # print('Dv {} days, {:.2f} weeks, {:.2f} months, {:.2f} years'.format(d.days, d.days / 7, d.days / 30, d.days / 365))

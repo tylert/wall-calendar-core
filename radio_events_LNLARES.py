8319eccc1811b5cbbbf9a6e24721d3a7ed647003
Tyler Tidman <tyler.tidman@draak.ca>
2020-12-03 18:06:36 -0500

    Fix up the comments in the LNL ARES net reminders a bit

--- radio_events_LNLARES.py
+++ radio_events_LNLARES.py
@@ -11,9 +11,8 @@ def main():
     '''
 
     for year in (2020, 2021, 2022, 2023):
-        # Rideau Lakes Amateur Radio Club nets are every Thursday of each month
-        # at 2000H
-        # https://www.ve3rlr.ca/p/about.html
+        # Lanark North Leeds ARES nets are every Wednesday of each month at
+        # 2000H.
         for month in range(1, 13):
             print(closest_day(WEDNESDAY, date(year, month, WEEK1)))
             print(closest_day(WEDNESDAY, date(year, month, WEEK2)))

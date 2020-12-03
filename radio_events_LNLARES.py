1eff617b5952792715a60af2baa1fd1ee65fc096
Tyler Tidman <tyler.tidman@draak.ca>
2020-12-03 17:47:04 -0500

    Fix up the net control list generators a bit more

--- radio_events_LNLARES.py
+++ radio_events_LNLARES.py
@@ -12,20 +12,16 @@ def main():
 
     for year in (2020, 2021, 2022, 2023):
         # Rideau Lakes Amateur Radio Club nets are every Thursday of each month
+        # at 2000H
         # https://www.ve3rlr.ca/p/about.html
         for month in range(1, 13):
-            print(closest_day(WEDNESDAY, date(year, month, WEEK1)), end='')
-            print(' 2000h LNL ARES Net')
-            print(closest_day(WEDNESDAY, date(year, month, WEEK2)), end='')
-            print(' 2000h LNL ARES Net')
-            print(closest_day(WEDNESDAY, date(year, month, WEEK3)), end='')
-            print(' 2000h LNL ARES Net')
-            print(closest_day(WEDNESDAY, date(year, month, WEEK4)), end='')
-            print(' 2000h LNL ARES Net')
+            print(closest_day(WEDNESDAY, date(year, month, WEEK1)))
+            print(closest_day(WEDNESDAY, date(year, month, WEEK2)))
+            print(closest_day(WEDNESDAY, date(year, month, WEEK3)))
+            print(closest_day(WEDNESDAY, date(year, month, WEEK4)))
             if closest_day(WEDNESDAY, date(year, month, WEEK4)) \
                     != closest_day(WEDNESDAY, date(year, month, WEEK4), last=True):
-                print(closest_day(WEDNESDAY, date(year, month, WEEK4), last=True), end='')
-                print(' 2000h LNL ARES Net')
+                print(closest_day(WEDNESDAY, date(year, month, WEEK4), last=True))
 
 
 if __name__ == '__main__':
--- radio_events_RLARC.py
+++ radio_events_RLARC.py
@@ -12,20 +12,16 @@ def main():
 
     for year in (2020, 2021, 2022, 2023):
         # Rideau Lakes Amateur Radio Club nets are every Thursday of each month
+        # at 1930H.
         # https://www.ve3rlr.ca/p/about.html
         for month in range(1, 13):
-            print(closest_day(THURSDAY, date(year, month, WEEK1)), end='')
-            print(' 1930h RLARC Net')
-            print(closest_day(THURSDAY, date(year, month, WEEK2)), end='')
-            print(' 1930h RLARC Net')
-            print(closest_day(THURSDAY, date(year, month, WEEK3)), end='')
-            print(' 1930h RLARC Net')
-            print(closest_day(THURSDAY, date(year, month, WEEK4)), end='')
-            print(' 1930h RLARC Net')
+            print(closest_day(THURSDAY, date(year, month, WEEK1)))
+            print(closest_day(THURSDAY, date(year, month, WEEK2)))
+            print(closest_day(THURSDAY, date(year, month, WEEK3)))
+            print(closest_day(THURSDAY, date(year, month, WEEK4)))
             if closest_day(THURSDAY, date(year, month, WEEK4)) \
                     != closest_day(THURSDAY, date(year, month, WEEK4), last=True):
-                print(closest_day(THURSDAY, date(year, month, WEEK4), last=True), end='')
-                print(' 1930h RLARC Net')
+                print(closest_day(THURSDAY, date(year, month, WEEK4), last=True))
 
 
 if __name__ == '__main__':

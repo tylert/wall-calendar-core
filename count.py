b5eaa8fb1ce82842731806311c15140fb70ac975
Tyler Tidman <tyler.tidman@draak.ca>
2021-08-12 10:55:18 -0400

    Fix up the countdown dashboard thing a bit

--- count.py
+++ count.py
@@ -8,19 +8,14 @@ today_date = date.today()
 # ---==[b]==---
 if today_date.month > 6 or (today_date.month == 6 and today_date.day > 15):
     b = date(today_date.year + 1, 6, 15) - today_date  # count down to next mid-June
-    print(f'Bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years')
 else:
     b = date(today_date.year, 6, 15) - today_date  # count down to this mid-June
-    print(f'Bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years')
-
-# ---==[o]==---
-o = today_date - date(2020, 3, 9)  # count up from date
-print(f'O^ {o.days} days, {o.days / 7:.2f} weeks, {o.days / 30:.2f} months, {o.days / 365:.2f} years')
+print(f'Bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years')
 
 # ---==[e]==---
 e = today_date - date(2017, 10, 10)  # count up from date
 print(f'E^ {e.days} days, {e.days / 7:.2f} weeks, {e.days / 30:.2f} months, {e.days / 365:.2f} years')
 
 # ---==[r]==---
-r = date(2021, 6, 1) + timedelta(days=365 * 10) - today_date  # count down to date ten years in the future
-print(f'Rv {r.days} days, {r.days / 7:.2f} weeks, {r.days / 30:.2f} months, {r.days / 365:.2f} years')
+# r = date(2021, 6, 1) + timedelta(days=365 * 10) - today_date  # count down to date ten years in the future
+# print(f'Rv {r.days} days, {r.days / 7:.2f} weeks, {r.days / 30:.2f} months, {r.days / 365:.2f} years')

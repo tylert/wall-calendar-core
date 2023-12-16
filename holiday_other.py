5785ae9b75ef8e958b7c0af8ccc8fb9336f9e96f
Tyler Tidman <tyler.tidman@draak.ca>
2023-12-16 18:00:12 -0500

    Nearly ready with the season stuff

--- holiday_other.py
+++ holiday_other.py
@@ -18,31 +18,37 @@ from paper_cal import *
 def main(year):
     ''' '''
 
-    #   https://en.wikipedia.org/wiki/Spring_break
-    #   https://fr.wikipedia.org/wiki/Semaine_de_rel%C3%A2che
-    # March Break
-    # Spring Break
-    # Congé de mars
-    # Congé de printemps
-    # Semaine de relâche
+    #// https://en.wikipedia.org/wiki/Spring_break
+    #// https://fr.wikipedia.org/wiki/Semaine_de_rel%C3%A2che
+    #// March Break
+    #// Spring Break
+    #// Congé de mars
+    #// Congé de printemps
+    #// Semaine de relâche
 
-    #   https://en.wikipedia.org/wiki/March_equinox
-    #   https://fr.wikipedia.org/wiki/%C3%89quinoxe_de_mars
-    #   https://en.wikipedia.org/wiki/June_solstice
-    #   https://en.wikipedia.org/wiki/September_equinox
-    #   https://en.wikipedia.org/wiki/December_solstice
+    #// https://en.wikipedia.org/wiki/March_equinox
+    #// https://fr.wikipedia.org/wiki/%C3%89quinoxe_de_mars
+    #// https://en.wikipedia.org/wiki/June_solstice
+    #// https://en.wikipedia.org/wiki/September_equinox
+    #// https://en.wikipedia.org/wiki/December_solstice
+    #// https://en.wikipedia.org/wiki/Winter_solstice
+    #// https://fr.wikipedia.org/wiki/Solstice_d%27hiver
+    #// Premier jour de printemps
+    #// Premier jour d'été
+    #// Premier jour d'automne
+    #// Premier jour d'hiver
     print(
         f'{spring(year).date()} {spring(year).time().strftime("%H:%M")} First day of Spring'
-    )  # Premier jour de printemps
+    )
     print(
         f'{summer(year).date()} {summer(year).time().strftime("%H:%M")} First day of Summer'
-    )  # Premier jour d'été
+    )
     print(
         f'{autumn(year).date()} {autumn(year).time().strftime("%H:%M")} First day of Fall'
-    )  # Premier jour d'automne
+    )
     print(
         f'{winter(year).date()} {winter(year).time().strftime("%H:%M")} First day of Winter'
-    )  # Premier jour d'hiver
+    )
 
     print(
         f'{perihelion(year).date()} {perihelion(year).time().strftime("%H:%M")} Perihelion'

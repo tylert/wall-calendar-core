49ab7fdd8f2ebec98ca8f3a78c7c19544ac01022
Tyler Tidman <tyler.tidman@draak.ca>
2024-01-02 09:45:45 -0500

    Minor tweaks

--- count.py
+++ count.py
@@ -6,6 +6,12 @@ from datetime import date, timedelta
 
 today_date = date.today()
 
+# ---==[s]==---
+s = today_date - date(2017, 10, 10)  # count up from a date
+print(
+    f'S^ {s.days} days, {s.days / 7:.2f} weeks, {s.days / 30:.2f} months, {s.days / 365:.2f} years'
+)
+
 # ---==[b]==---
 if today_date.month > 6 or (today_date.month == 6 and today_date.day > 15):
     b = date(today_date.year + 1, 6, 15) - today_date  # count down to next mid-June
@@ -16,11 +22,9 @@ print(
 )
 # v=10000;w=.1;x=1.1;y=1;z=.5;print(((v*.5*w*x)+(v*.5*w*x*y))*z)
 
-# ---==[s]==---
-s = today_date - date(2017, 10, 10)  # count up from a date
-print(
-    f'S^ {s.days} days, {s.days / 7:.2f} weeks, {s.days / 30:.2f} months, {s.days / 365:.2f} years'
-)
+# ---==[k]==---
+k = date(2024, 2, 16) - today_date  # count down to a date
+print(f'Kv {k.days} days, {k.days / 7:.2f} weeks, {k.days / 30:.2f} months, {k.days / 365:.2f} years')
 
 # ---==[a]==---
 a = date(2024, 4, 5) - today_date  # count down to a date

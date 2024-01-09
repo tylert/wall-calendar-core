b1da221d6cdb271a6254a06df348402364bcc541
Tyler Tidman <tyler.tidman@draak.ca>
2024-01-09 12:06:49 -0500

    Update weekday counter

--- count.py
+++ count.py
@@ -9,7 +9,7 @@ today_date = date.today()
 # ---==[s]==---
 s = today_date - date(2017, 10, 10)  # count up from a date
 print(
-    f'S^ {s.days * 5/7:.2f} workdays, {s.days} days, {s.days / 7:.2f} weeks, {s.days / 30:.2f} months, {s.days / 365:.2f} years'
+    f'S^ {s.days} days, {s.days / 7:.2f} weeks, {s.days / 30:.2f} months, {s.days / 365:.2f} years'
 )
 
 # ---==[b]==---
@@ -18,14 +18,22 @@ if today_date.month > 6 or (today_date.month == 6 and today_date.day > 15):
 else:
     b = date(today_date.year, 6, 15) - today_date  # count down to this mid-June
 print(
-    f'Bv {b.days * 5/7:.2f} workdays, {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years'
+    f'Bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years'
 )
 # v=10000;w=.1;x=1.1;y=1;z=.5;print(((v*.5*w*x)+(v*.5*w*x*y))*z)
 
 # ---==[k]==---
 k = date(2024, 2, 29) - today_date  # count down to a date
-print(f'Kv {k.days * 5/7:.2f} workdays, {k.days} days, {k.days / 7:.2f} weeks, {k.days / 30:.2f} months, {k.days / 365:.2f} years')
+print(f'Kv {k.days} days, {k.days / 7:.2f} weeks, {k.days / 30:.2f} months, {k.days / 365:.2f} years')
+
+d2 = date(2024, 2, 29)
+daygenerator = (today_date + timedelta(x + 1) for x in range((d2 - today_date).days)) # generate all days from d1 to d2
+print(f'   {sum(1 for day in daygenerator if day.weekday() < 5)} weekdays')
 
 # ---==[a]==---
 a = date(2024, 4, 5) - today_date  # count down to a date
-print(f'Av {a.days * 5/7:.2f} workdays, {a.days} days, {a.days / 7:.2f} weeks, {a.days / 30:.2f} months, {a.days / 365:.2f} years')
+print(f'Av {a.days} days, {a.days / 7:.2f} weeks, {a.days / 30:.2f} months, {a.days / 365:.2f} years')
+
+d2 = date(2024, 4, 5)
+daygenerator = (today_date + timedelta(x + 1) for x in range((d2 - today_date).days)) # generate all days from d1 to d2
+print(f'   {sum(1 for day in daygenerator if day.weekday() < 5)} weekdays')

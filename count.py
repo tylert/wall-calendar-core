7a1aa8d2a380894c62920c2b9c5360adef33aad8
Tyler Tidman <tyler.tidman@draak.ca>
2024-02-13 12:42:49 -0500

    Adjust countdown thingy

--- count.py
+++ count.py
@@ -4,36 +4,26 @@
 from datetime import date, timedelta
 
 
-today_date = date.today()
+t = date.today()
 
-# ---==[s]==---
-s = today_date - date(2017, 10, 10)  # count up from a date
+# ---==[e]==---
+e = t - date(2017, 10, 10)  # count up from a date
 print(
-    f'S^ {s.days} days, {s.days / 7:.2f} weeks, {s.days / 30:.2f} months, {s.days / 365:.2f} years'
+    f'e^ {e.days} days, {e.days / 7:.2f} weeks, {e.days / 30:.2f} months, {e.days / 365:.2f} years'
 )
 
 # ---==[b]==---
-if today_date.month > 6 or (today_date.month == 6 and today_date.day > 15):
-    b = date(today_date.year + 1, 6, 15) - today_date  # count down to next mid-June
+if t.month > 6 or (t.month == 6 and t.day > 15):
+    b = date(t.year + 1, 6, 15) - t  # count down to next mid-June
 else:
-    b = date(today_date.year, 6, 15) - today_date  # count down to this mid-June
+    b = date(t.year, 6, 15) - t  # count down to this mid-June
 print(
-    f'Bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years'
+    f'bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years'
 )
 # v=10000;w=.1;x=1.1;y=1;z=.5;print(((v*.5*w*x)+(v*.5*w*x*y))*z)
 
-# ---==[k]==---
-k = date(2024, 2, 29) - today_date  # count down to a date
-print(f'Kv {k.days} days, {k.days / 7:.2f} weeks, {k.days / 30:.2f} months, {k.days / 365:.2f} years')
-
-d2 = date(2024, 2, 29)
-daygenerator = (today_date + timedelta(x + 1) for x in range((d2 - today_date).days))
-print(f'   {sum(1 for day in daygenerator if day.weekday() < 5)} weekdays')
-
-# ---==[a]==---
-a = date(2024, 4, 5) - today_date  # count down to a date
-print(f'Av {a.days} days, {a.days / 7:.2f} weeks, {a.days / 30:.2f} months, {a.days / 365:.2f} years')
-
-d2 = date(2024, 4, 5)
-daygenerator = (today_date + timedelta(x + 1) for x in range((d2 - today_date).days))
-print(f'   {sum(1 for day in daygenerator if day.weekday() < 5)} weekdays')
+# ---==[f]==---
+f = date(2024, 4, 5) - t  # count down to a date
+print(f'fv {f.days} days, {f.days / 7:.2f} weeks, {f.days / 30:.2f} months, {f.days / 365:.2f} years')
+daygen = (t + timedelta(x + 1) for x in range(f.days))
+print(f'   {sum(1 for x in daygen if x.weekday() < 5)} weekdays')

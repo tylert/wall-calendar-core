363d45ee76e0d9f0e15ccbb3b8197fe559c1f593
Tyler Tidman <tyler.tidman@draak.ca>
2024-03-26 11:04:04 -0400

    Meh

--- count.py
+++ count.py
@@ -21,11 +21,11 @@ print(
     f'bv {b.days} days, {b.days / 7:.2f} weeks, {b.days / 30:.2f} months, {b.days / 365:.2f} years'
 )
 daygen = (t + timedelta(x + 1) for x in range(b.days))
-print(f'   {sum(1 for x in daygen if x.weekday() < 5)} weekdays')
+print(f'   {sum(1 for x in daygen if x.weekday() < 5)} workdays')
 # v=10000;w=.1;x=1.1;y=1;z=.5;print(((v*.5*w*x)+(v*.5*w*x*y))*z)
 
 # ---==[f]==---
 f = date(2024, 4, 5) - t  # count down to a date
 print(f'fv {f.days} days, {f.days / 7:.2f} weeks, {f.days / 30:.2f} months, {f.days / 365:.2f} years')
 daygen = (t + timedelta(x + 1) for x in range(f.days))
-print(f'   {sum(1 for x in daygen if x.weekday() < 5)} weekdays')
+print(f'   {sum(1 for x in daygen if x.weekday() < 5)} workdays')
